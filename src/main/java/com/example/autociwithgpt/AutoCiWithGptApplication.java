e04073a4410ade46320d069946ad830c09672d8d
PC HONG <peterhpc29@gmail.com>
2023-05-24 21:16:35 +0800

    fisrt task

--- src/main/java/com/example/autociwithgpt/AutoCiWithGptApplication.java
+++ src/main/java/com/example/autociwithgpt/AutoCiWithGptApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AutoCiWithGptApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutoCiWithGptApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AutoCiWithGptApplication.class, args);
+    }
 
 }
