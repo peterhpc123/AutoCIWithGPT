a6c88d85411a0be026867739cbe96a0c4ef7fc03
PC HONG <peterhpc29@gmail.com>
2023-05-24 21:12:51 +0800

    first commit

---
+++ src/main/java/com/example/autociwithgpt/AutoCiWithGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.autociwithgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCiWithGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCiWithGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autociwithgpt/AutoCiWithGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autociwithgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCiWithGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
